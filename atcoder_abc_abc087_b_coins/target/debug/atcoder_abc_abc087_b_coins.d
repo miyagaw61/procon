31ce455e2ea298b550f8dcc823945a43a2ce6172
miyagaw61 <miyagaw61@gmail.com>
2019-03-21 22:19:50 +0900

    Add atcoder_abc_abc087_b_coins

---
+++ atcoder_abc_abc087_b_coins/target/debug/atcoder_abc_abc087_b_coins.d
@@ -0,0 +1,1 @@
+/home/miyagaw61/src/github.com/miyagaw61/procon/atcoder_abc_abc087_b_coins/target/debug/atcoder_abc_abc087_b_coins: /home/miyagaw61/src/github.com/miyagaw61/procon/atcoder_abc_abc087_b_coins/src/main.rs
