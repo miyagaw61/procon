5a18154c63a6b1df8d07e523055039f95e5e73cd
miyagaw61 <miyagaw61@gmail.com>
2019-03-21 22:23:08 +0900

    Add atcoder_abc_abc083_b_some_sums

---
+++ atcoder_abc_abc083_b_some_sums/src/main.rs
@@ -0,0 +1,82 @@
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn get_one_digit(n: i32) -> i32 {
+    let one_digit = n % 10;
+    return one_digit;
+}
+
+fn main() {
+    input! {
+        n: i32,
+        a: i32,
+        b: i32
+    }
+    let mut sum = 0;
+    for x in 1..n+1 {
+        let mut d = x.clone();
+        let mut tmp_sum = 0;
+        loop {
+            let one_digit = get_one_digit(d);
+            tmp_sum = tmp_sum + one_digit;
+            d = d / 10;
+            if d == 0 {
+                break;
+            }
+        }
+        if a <= tmp_sum && tmp_sum <= b {
+            sum = sum + x;
+        }
+    }
+    println!("{}", sum);
+}
